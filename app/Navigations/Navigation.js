8327f92cef460f06ae03ff13e4eec6efb307618e
gerardo <gerardovillalpando1997@gmail.com>
2021-05-10 14:30:57 -0500

    version 1.0

---
+++ app/Navigations/Navigation.js
@@ -0,0 +1,50 @@
+import React from "react";
+import {NavigationContainer} from "@react-navigation/native";
+//import { createStackNavigator } from '@react-navigation/stack';
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import { Text } from 'react-native-elements';
+//import CuentaStack from "./CuentaStack";
+import Login from "../screens/Login"
+import Register from "../screens/Register"
+
+//import Cuenta from "../screens/Cuenta";
+import Home from "../screens/Home";
+import Settings from "../screens/Settings";
+import shouldUseActivityState from "react-native-screens";
+
+//const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+export default function navigation(){
+return(
+    <NavigationContainer>
+        
+      <Text h1></Text>
+        <Tab.Navigator>
+           
+            <Tab.Screen 
+            name ="Login" 
+            component={Login}
+            options ={{ title: " Mi Login"}}
+            />
+            
+            <Tab.Screen 
+            name ="Home" 
+            component={Home} 
+            options={{title: 'Home'}}/>
+           <Tab.Screen 
+            name ="Config" 
+            component={Settings} 
+            options={{title: 'Mi Configuracion'}}/>
+        </Tab.Navigator>
+        
+    
+        
+
+    </NavigationContainer>
+);
+
+}
+
+
+
