41f488d634682390e9d9ee3526e20b2b37ca4821
gerardo <gerardovillalpando1997@gmail.com>
2021-05-10 23:12:54 -0500

    version1.2

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Navigation from "./app/Navigations/Navigation";
-import shouldUseActivityState from "react-native-screens";
+
 
 export default function App  (){
 
