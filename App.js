c710c123a7e8300b636942e121da6322c5ab2455
GeardoThor97 <64027379+GeardoThor97@users.noreply.github.com>
2021-06-11 10:41:34 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,64 +1,20 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet } from "react-native";
 
-// Navigation
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+//import { Text } from 'react-native';
+import Login from './componentes/Login';
+import Menu from './componentes/Menu';
+import React, {useState} from 'react';
 
-// Components
-import CreateUserScreen from "./Screens/CreateUserScreen"
-import UserDetailScreen from "./Screens/UserDetailsScreen"
-import UsersList from "./Screens/UserList"
-//login funcionando
+function App () {
+  const [conectado, setConectado]= useState(false);
 
-const Stack = createStackNavigator();
+  const acceder = (estado)=>{
+    setConectado (estado)
+  }
 
-function MyStack() {
   return (
-    <Stack.Navigator
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: "#621FF7",
-        },
-        headerTintColor: "#fff",
-        headerTitleStyle: {
-          fontWeight: "bold",
-        },
-      }}
-    >
-      <Stack.Screen
-        name="UsersList"
-        component={UsersList}
-        options={{ title: "Users List" }}
-      />
-      <Stack.Screen
-        name="CreateUserScreen"
-        component={CreateUserScreen}
-        options={{ title: "Create a New User" }}
-      />
-      <Stack.Screen
-        name="UserDetailScreen"
-        component={UserDetailScreen}
-        options={{ title: "User Detail" }}
-      />
-    </Stack.Navigator>
+    conectado ? <Menu/> : <Login acceder = {acceder}/>
+    
   );
 }
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <MyStack />
-    </NavigationContainer>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default App;
